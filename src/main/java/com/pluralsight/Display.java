951bafe70f891980e0af6c8b4f84ab64b5b21c40
pmvroque15 <pmvroque@gmail.com>
2026-07-13 14:11:07 -0400

    Initialize MenuStrings class

--- src/main/java/com/pluralsight/Display.java
+++ src/main/java/com/pluralsight/Display.java
@@ -1,4 +1,9 @@
 package com.pluralsight;
 
+import com.pluralsight.model.MenuStrings;
+
 public class Display {
+    public static void main(String[] args) {
+        System.out.println(MenuStrings.HOME_MENU);
+    }
 }
