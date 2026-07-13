3fb3740c9a4b0803343693bb243a96619db5abc2
pmvroque15 <pmvroque@gmail.com>
2026-07-13 14:17:06 -0400

    Fixed MenuStrings, populates perfect now

--- src/main/java/com/pluralsight/Display.java
+++ src/main/java/com/pluralsight/Display.java
@@ -4,6 +4,6 @@ import com.pluralsight.model.MenuStrings;
 
 public class Display {
     public static void main(String[] args) {
-        System.out.println(MenuStrings.HOME_MENU);
+        System.out.println(MenuStrings.REPORTS_MENU);
     }
 }
--- src/main/java/com/pluralsight/model/MenuStrings.java
+++ src/main/java/com/pluralsight/model/MenuStrings.java
@@ -2,32 +2,32 @@ package com.pluralsight.model;
 
 public class MenuStrings {
     public static final String HOME_MENU = """
-                                    \\n╔═══════════════════╗
-                                    ║  \\u001B[1;33m==== HOME ====\\u001B[0m   ║
+                                    ╔═══════════════════╗
+                                    ║  \u001B[1;33m==== HOME ====\u001B[0m   ║
                                     ╠═══════════════════╣
-                                    ║ \\u001B[32m♦ D\\u001B[0m  Add Deposit  ║
-                                    ║ \\u001B[31m♦ P\\u001B[0m  Make Payment ║
-                                    ║ \\u001B[34m♦ L\\u001B[0m  Ledger       ║
-                                    ║ \\u001B[35m♦ X\\u001B[0m  Exit         ║
+                                    ║ \u001B[32m♦ D \u001B[0m  Add Deposit ║
+                                    ║ \u001B[31m♦ P\u001B[0m  Make Payment ║
+                                    ║ \u001B[34m♦ L\u001B[0m  Ledger       ║
+                                    ║ \u001B[35m♦ X\u001B[0m  Exit         ║
                                     ╚═══════════════════╝
-                                    \\u001B[36m?\\u001B[0m What would you like to do: \\s;
+                                    \u001B[36m?\u001B[0m What would you like to do: \s;
             
 """;
     public static final String  LEDGER_MENU = """
-                                    \\n╔═══════════════════╗
-                                    ║ \\u001B[1;33m==== LEDGER ====\\u001B[0m  ║
+                                    ╔═══════════════════╗
+                                    ║ \u001B[1;33m==== LEDGER ====\u001B[0m  ║
                                     ╠═══════════════════╣
-                                    ║ \\u001B[32m♦ A\\u001B[0m  All Entries  ║
-                                    ║ \\u001B[31m♦ D\\u001B[0m  Deposits     ║
-                                    ║ \\u001B[34m♦ P\\u001B[0m  Payments     ║
-                                    ║ \\u001B[35m♦ R\\u001B[0m  Reports      ║
-                                    ║ \\u001B[36m♦ H\\u001B[0m  Home         ║
+                                    ║ \u001B[32m♦ A\u001B[0m  All Entries  ║
+                                    ║ \u001B[31m♦ D\u001B[0m  Deposits     ║
+                                    ║ \u001B[34m♦ P\u001B[0m  Payments     ║
+                                    ║ \u001B[35m♦ R\u001B[0m  Reports      ║
+                                    ║ \u001B[36m♦ H\u001B[0m  Home         ║
                                     ╚═══════════════════╝
                                     \\u001B[36m?\\u001B[0m What would you like to do: \\s;
             
             """;
     public static final String REPORTS_MENU = """
-                                    \n╔═════════════════════════╗
+                                    ╔═════════════════════════╗
                                     ║     \u001B[1;33m==== REPORTS ====\u001B[0m   ║
                                     ╠═════════════════════════╣
                                     ║ \u001B[32m♦ 1\u001B[0m  Month To Date      ║
@@ -38,6 +38,6 @@ public class MenuStrings {
                                     ║ \u001B[36m♦  6\u001B[0m  Custom Search     ║
                                     ║ \u001B[37m♦ 0\u001B[0m  Back               ║
                                     ╚═════════════════════════╝
-                                    \u001B[36m?\u001B[0m What would you like to do: \\s;
+                                    \u001B[36m?\u001B[0m What would you like to do: \s;
 """;
 }
