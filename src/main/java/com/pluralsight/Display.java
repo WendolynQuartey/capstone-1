c4291cb7f4eedb2a9d1971f2a83dea42ba7683f1
pmvroque15 <pmvroque@gmail.com>
2026-07-13 20:43:57 -0400

    Edited and added comments in the Display Class

--- src/main/java/com/pluralsight/Display.java
+++ src/main/java/com/pluralsight/Display.java
@@ -10,30 +10,35 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class Display {
+    private static final Scanner scanner = new Scanner(System.in);
+    //Todo add an instance of Transactions
 
     public static void homeScreen() {
         boolean running = true;
-        do {
 
+        do {
             System.out.println(MenuStrings.HOME_MENU);
             String userSelection = scanner.nextLine();
 
-            switch (userSelection) {
+            switch (userSelection.toUpperCase()) {
                 // checks for user selection with case insensitivity
-                case "D", "d": {
-                    addDeposit();
+                case "D": {
+                    //AddDeposit method from transactions method
+//                    addDeposit();
                     break;
                 }
-                case "P", "p": {
-                    makePayment();
+                case "P": {
+                    //MakePayment method from payment method
+//                    makePayment();
                     break;
                 }
-                case "L", "l":
+                case "L":
                     displayLedger();
                     break;
-                case "X", "x": {
+                case "X": {
                     running = false;
                     break;
                 }
@@ -42,76 +47,82 @@ public class Display {
             }
         } while (running);
     }
+
+
     public static void displayLedger() {
-        try {
-            boolean running = true;
-            do {
-                BufferedReader buffReader = new BufferedReader(new FileReader(FILE_NAME));
+        boolean running = true;
+//        try {
+        //Todo: add to transactions class
+//
+//
+//                BufferedReader buffReader = new BufferedReader(new FileReader(FILE_NAME));
+//
+//                buffReader.readLine();
+//
+//
+//                String fileLine;
+//                transactions.clear();
+//                while ((fileLine = buffReader.readLine()) != null) {
+//                    String[] transactionInfo = fileLine.split("\\|");
+//                    Transaction newTransaction = new Transaction(
+//                            LocalDate.parse(transactionInfo[0]),
+//                            LocalTime.parse(transactionInfo[1]),
+//                            transactionInfo[2],
+//                            transactionInfo[3],
+//                            Double.parseDouble(transactionInfo[4])
+//                    );
+//                    transactions.add(newTransaction);
+//                }
+//
+//                // Compares the Transaction objects in my transactions array list by their dates and then their times
+//                Comparator<Transaction> transactionComparator = Comparator.comparing(Transaction::getTime).thenComparing(Transaction::getDate);
+//                transactions.sort(transactionComparator.reversed());
+        do {
+            System.out.println(MenuStrings.LEDGER_MENU);
+            String userChoice = scanner.nextLine();
 
-                buffReader.readLine();
+            switch (userChoice.toUpperCase()) {
+                case "A":
+//                for (Transaction t : transactions) {
+//                    System.out.println(t.displayTransaction());
+//                }
+//                pause();
+                    break;
+                case "D":
+//                for (Transaction t : transactions) {
+//                    if (t.getAmount() > 0) {
+//                        System.out.println(t.displayTransaction());
+//                    }
+//                }
+                    break;
+                case "P":
+//                for (Transaction t : transactions) {
+//                    if (t.getAmount() < 0) {
+//                        System.out.println(t.displayTransaction());
+//                    }
+//                }
+                    break;
+                case "R":
+                    displayReports();
+                    break;
+                case "H":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("That's not an option");
+            }
+//        buffReader.close();
+        } while (running);
 
-                System.out.println(MenuStrings.LEDGER_MENU);
-
-                String userChoice = scanner.nextLine();
-                String fileLine;
-                transactions.clear();
-                while ((fileLine = buffReader.readLine()) != null) {
-                    String[] transactionInfo = fileLine.split("\\|");
-                    Transaction newTransaction = new Transaction(
-                            LocalDate.parse(transactionInfo[0]),
-                            LocalTime.parse(transactionInfo[1]),
-                            transactionInfo[2],
-                            transactionInfo[3],
-                            Double.parseDouble(transactionInfo[4])
-                    );
-                    transactions.add(newTransaction);
-                }
-
-                // Compares the Transaction objects in my transactions array list by their dates and then their times
-                Comparator<Transaction> transactionComparator = Comparator.comparing(Transaction::getTime).thenComparing(Transaction::getDate);
-                transactions.sort(transactionComparator.reversed());
-
-                switch (userChoice) {
-                    case "A", "a":
-                        for (Transaction t : transactions) {
-                            System.out.println(t.displayTransaction());
-                        }
-                        pause();
-                        break;
-                    case "D", "d":
-                        for (Transaction t : transactions) {
-                            if (t.getAmount() > 0) {
-                                System.out.println(t.displayTransaction());
-                            }
-                        }
-                        break;
-                    case "P", "p":
-                        for (Transaction t : transactions) {
-                            if (t.getAmount() < 0) {
-                                System.out.println(t.displayTransaction());
-                            }
-                        }
-                        break;
-                    case "R", "r":
-                        displayReports();
-                        break;
-                    case "H", "h":
-                        running = false;
-                        break;
-                    default:
-                        System.out.println("That's not an option");
-                }
-                buffReader.close();
-            } while (running);
-        } catch (FileNotFoundException e) {
-            System.err.println("File cannot be located!");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File cannot be located!");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     //pause after a transaction to allow user time to read
-    static void pause() {
+    public static void pause() {
         String input = "";
         while (!input.equalsIgnoreCase("B")) {
             System.out.println();
@@ -122,6 +133,7 @@ public class Display {
 
     public static void displayReports() {
         boolean running = true;
+
         do {
 
             System.out.println(MenuStrings.REPORTS_MENU);
@@ -131,52 +143,52 @@ public class Display {
 
             switch (reportChoice) {
                 case 1:
-                    for (Transaction t : transactions) {
-                        LocalDate today = LocalDate.now();
-                        int thisMonth = today.getMonthValue();
-                        int thisYear = today.getYear();
-                        if (t.getDate().getMonthValue() == thisMonth && t.getDate().getYear() == thisYear) {
-                            System.out.println(t.displayTransaction());
-                        }
-                    }
+//                    for (Transaction t : transactions) {
+//                        LocalDate today = LocalDate.now();
+//                        int thisMonth = today.getMonthValue();
+//                        int thisYear = today.getYear();
+//                        if (t.getDate().getMonthValue() == thisMonth && t.getDate().getYear() == thisYear) {
+//                            System.out.println(t.displayTransaction());
+//                        }
+//                    }
                     break;
                 case 2:
-                    for (Transaction t : transactions) {
-                        LocalDate today = LocalDate.now();
-                        int lastMonth = today.getMonthValue() - 1;
-                        int thisYear = today.getYear();
-                        if (t.getDate().getMonthValue() == lastMonth && t.getDate().getYear() == thisYear) {
-                            System.out.println(t.displayTransaction());
-                        }
-                    }
+//                    for (Transaction t : transactions) {
+//                        LocalDate today = LocalDate.now();
+//                        int lastMonth = today.getMonthValue() - 1;
+//                        int thisYear = today.getYear();
+//                        if (t.getDate().getMonthValue() == lastMonth && t.getDate().getYear() == thisYear) {
+//                            System.out.println(t.displayTransaction());
+//                        }
+//                    }
                     break;
                 case 3:
-                    for (Transaction t : transactions) {
-                        LocalDate today = LocalDate.now();
-                        int thisYear = today.getYear();
-                        if (t.getDate().getYear() == thisYear && t.getDate().isBefore(today)) {
-                            System.out.println(t.displayTransaction());
-                        }
-                    }
+//                    for (Transaction t : transactions) {
+//                        LocalDate today = LocalDate.now();
+//                        int thisYear = today.getYear();
+//                        if (t.getDate().getYear() == thisYear && t.getDate().isBefore(today)) {
+//                            System.out.println(t.displayTransaction());
+//                        }
+//                    }
                     break;
                 case 4:
-                    for (Transaction t : transactions) {
-                        LocalDate today = LocalDate.now();
-                        int lastYear = today.getYear() - 1;
-                        if (t.getDate().getYear() == lastYear) {
-                            System.out.println(t.displayTransaction());
-                        }
-                    }
+//                    for (Transaction t : transactions) {
+//                        LocalDate today = LocalDate.now();
+//                        int lastYear = today.getYear() - 1;
+//                        if (t.getDate().getYear() == lastYear) {
+//                            System.out.println(t.displayTransaction());
+//                        }
+//                    }
                     break;
                 case 5:
                     System.out.print("\nWhich vendor would you like to search? ");
                     String searchVendor = scanner.nextLine();
 
-                    for (Transaction t : transactions) {
-                        if (t.getVendor().equalsIgnoreCase(searchVendor)) {
-                            System.out.println(t.displayTransaction());
-                        }
-                    }
+//                    for (Transaction t : transactions) {
+//                        if (t.getVendor().equalsIgnoreCase(searchVendor)) {
+//                            System.out.println(t.displayTransaction());
+//                        }
+//                    }
                     break;
                 case 0:
                     running = false;
